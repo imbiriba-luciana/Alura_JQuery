73db5221155aaa0e2b45a2081f4654a5386400f0
imbiriba-luciana <imbiriba.luciana@gmail.com>
2022-05-09 15:28:06 +0200

    parte 2

--- jQuery_Parte2/public/js/frase.js
+++ jQuery_Parte2/public/js/frase.js
@@ -3,15 +3,21 @@ $("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
 	$("#spinner").toggle();
+    /*o SPINNER (barra giratória de carregamento) vai ser uma barra de carregamento,
+        que será um feedback visual para o usuário saber que está acontecendo alguma coisa quando ele clica no botão */
     $.get("http://localhost:3000/frases", trocaFraseAleatoria)
-    .fail(function(){
+    .fail(function(){ /*Função do jQuery para lidar com a requisição AJAX quando ela falhar. */
     	$("#erro").toggle();
-    	setTimeout(function(){
-    		$("#erro").toggle();
+    	setTimeout(function(){ /*o SETTIMEOUT é para a msg de erro aparecer, mas depois sumir, não ficar direto*/
+    		$("#erro").toggle(); /*Para mostrar a msg de erro somente se a requisição falhar*/
     	},1500);
     })
     .always(function(){
-    	$("#spinner").toggle();
+    	$("#spinner").toggle(); 
+    /*na intenção de esconder o spinner quando a requisição terminar,
+    e considerando que a função "$.get" tem dois possíveis resultados (a requisição dar certo ou falhar),
+    a função ".always" do jQuery sempre vai executar o que for passado a ela, independente de o resultado da função get
+    ter dado certo ou errado. Então pode passar a função de esconder o spinner a ela através do toggle */
     });
 }
 
@@ -24,14 +30,45 @@ function trocaFraseAleatoria(data) {
     atualizaTempoInicial(data[numeroAleatorio].tempo);
 }
 
+/*o AJAX vai buscar informações em um servidor, onde pra essa finalidade aqui terá frases pré-prontas,
+para que as frases possam ficar mudando dentro do jogo. 
+o AJAX faz uma requisição ao servidor de modo assíncrona.
+Assim, toda vez que o jogador recarregar a página, o AJAX busca uma nova frase.
+De modo assíncrona significa que não irá prejudicar o fluxo da aplicação (???) */
+
+/*O projeto precisa, então, de uma outra pasta além da "public", que será a "servidor".
+Essa vai conter o servidor em NODE.JS, que será instanciado na máquina local.
+E ele serve para levantar o servidor, que por sua vez passará a segurar a aplicação.
+
+Ou seja, o site passará a funcionar a partir de um servidor,
+onde o "principal.html" será hosteado e carregado desse servidor,
+onde tb estão as frases salvas no backend do servidor para o "principa.html.
+
+Por debaixo dos panos, o navegador utiliza um verbo do HTTP para buscar os dados e exibí-los na tela.
+Contudo, se fizermos isso em nossa aplicação Alura Typer nossa página recarregará e nossa aplicação desaparecerá.
+É por isso que ao invés de pedirmos esses dados através do navegador, pedimos através do JavaScript,
+mais notadamente através de uma requisição ajax. Essa requisição não recarrega a tela,
+contudo é responsabilidade do programador obter os dados retornados e atualizar a página programaticamente
+através de JavaScript sem perturbar o fluxo da aplicação.
+
+Ou seja, o jQuery também serve de ponte de ligação do navegador com algum servidor.*/
+
+/*Como vamos trabalhar bastante com requisições AJAX neste módulo, precisamos levantar nosso servidor web. 
+Fazemos isto navegando pelo terminal até a pasta /alura-typer/servidor/ e inicializando o servidor com o comando npm start. 
+O terminal irá congelar, mas isto é normal, só significa que ele está rodando corretamente,
+devemos deixá-lo aberto enquanto estamos utilizando a aplicação.
+
+Devemos sempre acessar a página do Alura Typer através do endereço localhost:3000/principal.html. */
+
+/*com também AJAX conseguimos enviar dados para servidor tanto via POST como com GET também*/
 
 function buscaFrase() {
 
     $("#spinner").toggle();
-    var fraseId = $("#frase-id").val();
+    var fraseId = $("#frase-id").val(); /*para pegar o valor e enviar pro servidor*/
 
     //criacao do objeto JS que guarda a id
-    var dados = {id : fraseId}; 
+    var dados = {id : fraseId}; /*A fim de enviar os dados para o servidor de qual frase o AJAX vai lá buscar*/
 
     //passando objecto como segundo parametro
     $.get("http://localhost:3000/frases", dados, trocaFrase)
@@ -55,3 +92,4 @@ function trocaFrase(data) {
     atualizaTamanhoFrase();
     atualizaTempoInicial(data.tempo);
 }
+
--- jQuery_Parte2/public/js/placar.js
+++ jQuery_Parte2/public/js/placar.js
@@ -11,7 +11,9 @@ function inserePlacar() {
 
     corpoTabela.append(linha);
     $(".placar").slideDown(500);
-    scrollPlacar();
+    /*A função slideDown aceita receber como parâmetro um tempo em milissegundos.
+    Neste exemplo, a transição demorará 500 segundos para ser concluída. */
+    scrollPlacar(); 
 }
 
 function scrollPlacar() {
@@ -21,6 +23,13 @@ function scrollPlacar() {
         scrollTop: posicaoPlacar + "px"
     }, 1000);
 }
+/*Pra saber para onde eu quero dar scroll, é só abrir o DevTools,
+e no console dar o comando: o $(".placar").offset() 
+Ou então apenas: $(".placar").offset().top
+Que vai dar apenas a distância em relação ao topo da página.
+Por isso, após descobrir a posição de referência, é só informar numa função,
+e dar .ANIMATE, informar a propriedade SCROLLTOP,
+então dar os parâmetros (o deslizamento em "px", e os milissegundos)*/
 
 function novaLinha(usuario, palavras) {
     var linha = $("<tr>");
@@ -49,17 +58,48 @@ function removeLinha() {
     linha.fadeOut(1000);
     setTimeout(function() {
         linha.remove();
+        /* .FADEOUT serve para ir sumindo um elemento diminuindo sua opacidade,
+        onde 1000 é o tempo que dura a execução.
+        Mas requer o .REMOVE pra ser removido, de fato, mas pra que essa segunda função não seja executada
+        antes de FADEOUT terminar, precisa ser controlada por um SETTIMEOUT.
+        
+        Também existe FADEIN, FADEOUT, e FADETOGGLE*/
     }, 1000);
 }
 
 function mostraPlacar() {
-    $(".placar").stop().slideToggle(600);
+    $(".placar").stop().slideToggle(600); 
+    /* a função .STOP serve para parar a animação que estiver fazendo no momento e dá sequência.
+    Assim, ajuda a controlar melhor as animações, evitando que fiquem encadeadas uma atrás da outra,
+    e causem uma usabilidade esquisita*/
 }
 
-function sincronizaPlacar(){
+/*A função .stop() serve para interromper uma animação antes de começar a próxima. 
+Quando utilizamos a chamada de .stop() antes de invocar o proxímo .slideUp/Down ,
+fazemos com que a animação anterior seja interrompida antes de dar início a próxima,
+o que impede que várias animações fiquem encadeadas uma nas outras e crie um menu que fica
+subindo e descendo constantemente caso o usuário acione repetidas vezes os eventos. */
 
-    var placar = [];
-    var linhas = $("tbody>tr");
+/*O jQuery possui a função is que permite consultar uma pseudo class. 
+Toda vez que um elemento esta com display diferente de none ele ganha a pseudo classe :visible . 
+A função is retorna true caso o elemento esteja visível. Se ele estiver visível,
+precisamos escondê-lo e isso é feito através da função hide. Para exibir o elemento, é usada a função show. 
+
+A função toogleClass (não confundir com a função toggle) adiciona uma classe caso ela não exista no elemento. 
+Se existir, ela remove a classe.
+
+A função toggle é um atalho para as funções hide e show. 
+Quando ela é chamada para um elemento visível, o elemento fica invisível.
+Quando é chamada para um elemento invisível, ela torna o elemento visível.
+
+Esta correta! o jQuery possui a função hasClass que retorna true se um elemento possui ou não uma classe. 
+Na condição, removemos a classe invisivel caso o elemento já a tenha e a adicionamos caso ele não a tenha. 
+Todo esse processo é feito a cada clique do usuário.*/
+
+function sincronizaPlacar(){
+/*Lembrando que é sempre bom verificar se o botão está funcionando direito através do console.log*/
+    var placar = []; 
+    var linhas = $("tbody>tr"); /*Me vê todas as tr que são filhas diretas de tbody */
 
     linhas.each(function(){
         var usuario = $(this).find("td:nth-child(1)").text();
